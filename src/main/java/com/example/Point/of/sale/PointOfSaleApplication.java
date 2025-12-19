d78f6eab0a834abfca34688e7d406a622b34a3a1
Y0unesLAKSIR <laksir.younes1@gmail.com>
2025-12-19 09:13:50 +0100

    a

--- src/main/java/com/example/Point/of/sale/PointOfSaleApplication.java
+++ src/main/java/com/example/Point/of/sale/PointOfSaleApplication.java
@@ -3,10 +3,16 @@ package com.example.Point.of.sale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PointOfSaleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PointOfSaleApplication.class, args);
 	}
+
+    @GetMapping
+    public String hello() {
+        return "Hello from New Test :)";
+    }
 }
